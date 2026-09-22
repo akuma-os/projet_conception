9bf574a1b17081782c6e7b5d7715e53e0ad159ec
ilias <iliaslaaloua@gmail.com>
2026-09-22 12:08:05 +0200

    Ajout classe Pacman

---
+++ src/Main/Java/fr/ul/acl/Model/Pacman.java
@@ -0,0 +1,28 @@
+package Main.Java.fr.ul.acl.Model;
+public class Pacman {
+	private int x;
+	private int y;
+	public Pacman(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public void up() {
+		this.y++;
+	}
+	public void down() {
+		this.y--;
+	}
+	public void right() {
+		this.x++;
+	}
+	public void left() {
+		this.x--;
+	}
+	
+}
